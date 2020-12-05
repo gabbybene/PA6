4ea8f2abab068297443736611ba5a3e91095afab
Gabby Benefield <gnmoody@gmail.com>
2020-12-05 13:05:52 -0600

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PA6
+{
+    class Book
+    {
+        public String CWID { get; set; }
+        public String isbn { get; set; }
+        public String title { get; set; }
+        public String author { get; set; }
+        public String cover { get; set; }
+        public String genre { get; set; }
+        public int length { get; set; }
+        public int copies { get; set; }
+        public String _id { get; set; }
+
+        public Book(String CWID, String isbn, String title, String author, String cover, String genre, int length, int copies, String id)
+        {
+            this.CWID = CWID;
+            this.isbn = isbn;
+            this.title = title;
+            this.author = author;
+            this.cover = cover;
+            this.genre = genre;
+            this.length = length;
+            this.copies = copies;
+            _id = id;
+        }
+
+        public Book()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return this.title;
+        }
+    }
+}
---
+++ frmEdit.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PA6
+{
+    public partial class frmEdit : Form
+    {
+        private Book myBook;
+        private string CWID;
+        private string mode;
+        public frmEdit(Object tempBook, string tempMode, string tempCwid)
+        {
+            myBook = (Book)tempBook;
+            CWID = tempCwid;
+            mode = tempMode;
+            InitializeComponent();
+
+            pbCover.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void frmEdit_Load(object sender, EventArgs e)
+        {
+            if(mode == "edit")
+            {
+                txtTitleData.Text = myBook.title;
+                txtAuthorData.Text = myBook.author;
+                txtGenreData.Text = myBook.genre;
+                txtISBNData.Text = myBook.isbn;
+                txtCopiesData.Text = myBook.copies.ToString();
+                txtLengthData.Text = myBook.length.ToString();
+                txtCoverData.Text = myBook.cover;
+
+                pbCover.Load(myBook.cover);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            myBook.title = txtTitleData.Text;
+            myBook.author = txtAuthorData.Text;
+            myBook.genre = txtGenreData.Text;
+            myBook.copies = int.Parse(txtCopiesData.Text);
+            myBook.isbn = txtISBNData.Text;
+            myBook.cover = txtCoverData.Text;
+            myBook.length = int.Parse(txtLengthData.Text);
+            myBook.CWID = CWID;
+
+            BookFile.SaveBook(myBook, CWID, mode);
+
+            MessageBox.Show("Content was saved", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Close();
+        }
+    }
+}
